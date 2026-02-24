5ee6fe838f0a174f1c4be0c25b4daed9f8e12b34
Adedayo <adeboadedayo01@gmail.com>
2026-02-24 13:16:55 +0100

    changes

--- tesseract_update.py
+++ tesseract_update.py
@@ -1,6 +1,7 @@
 from pdf2image import convert_from_path #type: ignore
 import pytesseract #type: ignore
 from PIL import Image, ImageFilter #type: ignore
+import pypdf #type: ignore
 
 # Converting page 101 to an image
 print("Converting page 101 to image...")
@@ -27,3 +28,11 @@ text = pytesseract.image_to_string(img)
 
 print("--- TEXT EXTRACTED BY TESSERACT ---")
 print(text)
+
+# the result was better but didn't quite extract the table data perfectly
+#pypdf did this perfectly
+
+print("\n--- SAME PAGE WITH pypdf ---")
+reader = pypdf.PdfReader("/Users/apple/Downloads/Introductory_Statistics_2e_-_WEB.pdf")
+text = reader.pages[100].extract_text()
+print(text[:500])
