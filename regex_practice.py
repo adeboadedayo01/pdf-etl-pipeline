3405339547d80bbf02ba6015bde07f529ddd05c1
Adedayo <adeboadedayo01@gmail.com>
2026-02-26 17:44:28 +0100

    changes

--- regex_practice.py
+++ regex_practice.py
@@ -3,10 +3,9 @@ import re
 text = "Chapter 1 Sampling and Data"
 
 # Search for the word "Chapter" in the text
-result = re.search("Data", text)
+# The \d means "any digit"
+result = re.search(r"Chapter (\d+)", text)
 
 if result:
-    print("Found it!")
-    print(result)
-else:
-    print("Not found")
+    print("Found:", result.group(0))   # the full match
+    print("Chapter number:", result.group(1))  # just the number
